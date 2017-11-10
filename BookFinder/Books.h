c9cba4b5436e948aa0b42440fbb4dd004e0afdc5
YoganandP <Yoganandpathak@gmail.com>
2017-11-10 16:19:24 -0500

    Added code for the class Books

--- BookFinder/Books.h
+++ BookFinder/Books.h
@@ -1,1 +1,33 @@
 #pragma once
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+class Books {
+private:
+	string name;
+	Mat image = imread(name);
+	ifstream details;
+
+public:
+	Books(string);
+	Mat getImage();
+	void output();
+};
+
+Books::Books(string book_name) {
+	name = book_name;
+}
+
+Mat Books::getImage() {
+	return image;
+}
+
+void Books::output() {
+	details.open(name + ".txt");
+	if (details.is_open())
+		cout << details.rdbuf();
+}
+
