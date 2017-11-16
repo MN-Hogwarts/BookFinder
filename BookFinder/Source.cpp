d121ad912283390c98b8486f1783c840a3cacda1
Mrinali <mrinalinakhre.7@gmail.com>
2017-11-15 23:37:06 -0500

    commit

--- BookFinder/Source.cpp
+++ BookFinder/Source.cpp
@@ -6,7 +6,6 @@
 #include "opencv2/highgui.hpp"
 #include "opencv2/calib3d.hpp"
 #include "opencv2/xfeatures2d.hpp"
-//#include "Books.h"
 #include "Books.h"
 using namespace std;
 using namespace cv;
